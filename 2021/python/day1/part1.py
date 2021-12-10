2c8c1c59f553a7919ea6719b90b14ad6534d6580
Isaac Pang <isaac.pang91@gmail.com>
2021-12-11 10:43:38 +1100

    Update files as modules

--- 2021/python/day1/part1.py
+++ 2021/python/day1/part1.py
@@ -1,20 +1,33 @@
 from pathlib import Path
+from os import chdir
 
-path_to_file = Path("../../docs/day1/data.dat")
 
-with open(path_to_file, 'r') as file:
-    ref_data = next(file).strip('\n')
-    num_data = 1
-    greater_counter = 0
-    for data_point in file:
-        data_point = data_point.strip('\n')
-        if isinstance(data_point, str) and not data_point:
-            continue
+def main():
+    """Main function."""
+    path_to_file = Path("../../docs/day1/data.dat")
 
-        num_data += 1
-        if int(data_point) > int(ref_data):
-            greater_counter += 1
-        ref_data = data_point
+    with open(path_to_file, "r") as file:
+        ref_data = next(file).strip("\n")
+        num_data = 1
+        greater_counter = 0
+        for data_point in file:
+            data_point = data_point.strip("\n")
+            if isinstance(data_point, str) and not data_point:
+                continue
 
-    print('Measurements that are larger than the previous:', greater_counter)
-    print('Data in file: ', num_data)
+            num_data += 1
+            if int(data_point) > int(ref_data):
+                greater_counter += 1
+            ref_data = data_point
+
+        print("Measurements that are larger than the previous:", greater_counter)
+        print("Data in file: ", num_data)
+
+
+if __name__ == "__main__":
+    exec_filepath = str(Path(__file__))
+    exec_dir = "/".join(exec_filepath.split("/")[:-1])
+
+    chdir(exec_dir)
+
+    main()
--- 2021/python/day1/part2.py
+++ 2021/python/day1/part2.py
@@ -1,23 +1,36 @@
 from pathlib import Path
+from os import chdir
 
-path_to_file = Path("../../docs/day1/data.dat")
 
-with open(path_to_file, 'r', encoding='utf-8') as file:
-    data = file.read().splitlines()
+def main():
+    """Main function."""
+    path_to_file = Path("../../docs/day1/data.dat")
 
-def sum_triplet(x, y, z):
-    x = int(x)
-    y = int(y)
-    z = int(z)
-    return x + y + z
+    with open(path_to_file, "r", encoding="utf-8") as file:
+        data = file.read().splitlines()
 
-prev_sum = sum_triplet(data[0], data[1], data[2])
-greater_counter = 0
-for triplet in zip(data[1:-2], data[2:-1], data[3:]):
-    this_sum = sum_triplet(*triplet)
-    if this_sum > prev_sum:
-        greater_counter += 1
+    def sum_triplet(x, y, z):
+        x = int(x)
+        y = int(y)
+        z = int(z)
+        return x + y + z
 
-    prev_sum = this_sum
+    prev_sum = sum_triplet(data[0], data[1], data[2])
+    greater_counter = 0
+    for triplet in zip(data[1:-2], data[2:-1], data[3:]):
+        this_sum = sum_triplet(*triplet)
+        if this_sum > prev_sum:
+            greater_counter += 1
 
-print("Triplet sums larger than previous:", greater_counter)
+        prev_sum = this_sum
+
+    print("Triplet sums larger than previous:", greater_counter)
+
+
+if __name__ == "__main__":
+    exec_filepath = str(Path(__file__))
+    exec_dir = "/".join(exec_filepath.split("/")[:-1])
+
+    chdir(exec_dir)
+
+    main()
