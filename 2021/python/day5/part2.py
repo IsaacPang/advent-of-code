1739b5033472fe62ddd0530d331ff1edf9460bb5
Isaac Pang <isaac.pang91@gmail.com>
2021-12-15 00:14:56 +1100

    Start part 2 day 5

---
+++ 2021/python/day5/part2.py
@@ -0,0 +1,16 @@
+"""
+Module to solve part 2 of day 5 advent of code 2021.
+"""
+from pathlib import Path
+from os import chdir
+
+
+def main():
+    """Main function."""
+
+
+if __name__ == "__main__":
+    EXEC_FILEPATH = str(Path(__file__))
+    EXEC_DIR = "/".join(EXEC_FILEPATH.split("/")[:-1])
+    chdir(EXEC_DIR)
+    main()
