20728e561f4ddf4f28e35ccb4d8aad8ad9a2977a
Isaac Pang <isaac.pang91@gmail.com>
2021-12-15 00:56:18 +1100

    Solved part 1 day 6

--- 2021/python/day6/part1.py
+++ 2021/python/day6/part1.py
@@ -5,8 +5,30 @@ from pathlib import Path
 from os import chdir
 
 
+def decrease_one(value: int) -> int:
+    """Decrement an int by one."""
+    return value - 1
+
+
 def main():
     """Main function."""
+    path_to_file = Path("../../docs/day6/data.dat")
+    with open(path_to_file, "r") as file:
+        fishes = [int(value) for value in file.read().strip("\n").split(",")]
+
+    days_after = 0
+    fishes_to_add = []
+    while days_after < 80:
+        fishes += fishes_to_add
+        fishes_to_add = []
+        fishes = [decrease_one(fish) for fish in fishes]
+        for index, fish in enumerate(fishes):
+            if fish == 0:
+                fishes[index] = 7
+                fishes_to_add.append(9)
+        days_after += 1
+
+    print(len(fishes))
 
 
 if __name__ == "__main__":
